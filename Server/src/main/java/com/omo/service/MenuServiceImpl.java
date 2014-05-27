d59d8913ec2f5878fbe7fc32af30295cc05d02dc
Mark Haney <haneyman@yahoo.com>
2014-05-26 23:10:49 -0700

    added reorder button and background photo

--- Server/src/main/java/com/omo/service/MenuServiceImpl.java
+++ Server/src/main/java/com/omo/service/MenuServiceImpl.java
@@ -228,8 +228,8 @@ public class MenuServiceImpl implements MenuService {
         LocalDateTime now = new LocalDateTime();
         if (result) {
             DateTime nowPacific = now.toDateTime(DateTimeZone.forID("US/Pacific"));
-//            LocalTime deadline = new LocalTime(10, 00);//limit to 10:00 am
-            LocalTime deadline = new LocalTime(23, 00);//no limit
+            LocalTime deadline = new LocalTime(10, 00);//limit to 10:00 am
+//            LocalTime deadline = new LocalTime(23, 00);//no limit
             if (nowPacific.toLocalTime().isBefore(deadline))
                 result = true;
             else
